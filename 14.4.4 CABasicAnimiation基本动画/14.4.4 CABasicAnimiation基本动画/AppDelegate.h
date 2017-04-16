1032721d481c224ba0a39d90d5c39fdbbfb15465
test <422538722@qq.com>
2017-04-16 16:28:29 +0800

    14.4.4
     CABasicAnimiation基本动画

---
+++ 14.4.4 CABasicAnimiation基本动画/14.4.4 CABasicAnimiation基本动画/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  14.4.4 CABasicAnimiation基本动画
+//
+//  Created by 李维佳 on 2017/4/7.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
